63f0b1cda92992a2d3bb734f3743724c61cbafc8
Ayelen Garcia <ayelengarcia7@hotmail.com>
2025-11-13 13:55:18 -0300

    EDIT CARRUSEL

--- src/components/Carrusel/Carrusel.jsx
+++ src/components/Carrusel/Carrusel.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -5,15 +6,12 @@ import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import { Carrusel } from "./Carrusel.styled";
 
 const Carrousel = () => {
-
-  // eslint-disable-next-line react/prop-types
   const PrevArrow = ({ onClick }) => (
     <div className="slick-arrow arrow-prev" onClick={onClick}>
       <FaArrowLeft />
     </div>
   );
 
-  // eslint-disable-next-line react/prop-types
   const NextArrow = ({ onClick }) => (
     <div className="slick-arrow arrow-next" onClick={onClick}>
       <FaArrowRight />
@@ -24,7 +22,7 @@ const Carrousel = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,        // Por defecto Desktop
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2500,
@@ -32,55 +30,134 @@ const Carrousel = () => {
     nextArrow: <NextArrow />,
     responsive: [
       {
-        breakpoint: 1200,   // <= 1100px → Tablet
+        breakpoint: 768, // mobile
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 1,
+          arrows: false,
         },
       },
       {
-        breakpoint: 769,    // <= 769px → Mobile
+        breakpoint: 1200, // tablet
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 2,
           slidesToScroll: 1,
         },
       },
     ],
   };
 
-
-
   return (
     <Carrusel>
-      <Slider {...settings} className="m-0 mb-4 px-0">
-
-          <div className="container-novios">
-            <img src="/assets/foto1.jpg" alt="Logo" className="novios" />
-          </div>
-
-          <div className="container-novios">
-            <img src="/assets/foto2.jpg" alt="Logo" className="novios" />
-          </div>
-
-          <div className="container-novios">
-            <img src="/assets/foto3.jpg" alt="Logo" className="novios" />
-        </div>
-        
+      <Slider {...settings}>
         <div className="container-novios">
           <img src="/assets/foto1.jpg" alt="Logo" className="novios" />
         </div>
-
         <div className="container-novios">
           <img src="/assets/foto2.jpg" alt="Logo" className="novios" />
         </div>
-
         <div className="container-novios">
           <img src="/assets/foto3.jpg" alt="Logo" className="novios" />
         </div>
-     
+        <div className="container-novios">
+          <img src="/assets/foto1.jpg" alt="Logo" className="novios" />
+        </div>
+        <div className="container-novios">
+          <img src="/assets/foto2.jpg" alt="Logo" className="novios" />
+        </div>
+        <div className="container-novios">
+          <img src="/assets/foto3.jpg" alt="Logo" className="novios" />
+        </div>
       </Slider>
     </Carrusel>
   );
 };
 
 export default Carrousel;
+
+// import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+// import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+// import { Carrusel } from "./Carrusel.styled";
+
+// const Carrousel = () => {
+
+//   // eslint-disable-next-line react/prop-types
+//   const PrevArrow = ({ onClick }) => (
+//     <div className="slick-arrow arrow-prev" onClick={onClick}>
+//       <FaArrowLeft />
+//     </div>
+//   );
+
+//   // eslint-disable-next-line react/prop-types
+//   const NextArrow = ({ onClick }) => (
+//     <div className="slick-arrow arrow-next" onClick={onClick}>
+//       <FaArrowRight />
+//     </div>
+//   );
+
+//   const settings = {
+//     dots: true,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 3,        // Por defecto Desktop
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2500,
+//     prevArrow: <PrevArrow />,
+//     nextArrow: <NextArrow />,
+//     responsive: [
+//       {
+//         breakpoint: 1200,   // <= 1100px → Tablet
+//         settings: {
+//           slidesToShow: 2,
+//           slidesToScroll: 1,
+//         },
+//       },
+//       {
+//         breakpoint: 769,    // <= 769px → Mobile
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//         },
+//       },
+//     ],
+//   };
+
+
+
+//   return (
+//     <Carrusel>
+//       <Slider {...settings} className="m-0 mb-4 px-0">
+
+//         <div className="container-novios">
+//           <img src="/assets/foto1.jpg" alt="Logo" className="novios" />
+//         </div>
+
+//         <div className="container-novios">
+//           <img src="/assets/foto2.jpg" alt="Logo" className="novios" />
+//         </div>
+
+//         <div className="container-novios">
+//           <img src="/assets/foto3.jpg" alt="Logo" className="novios" />
+//         </div>
+
+//         <div className="container-novios">
+//           <img src="/assets/foto1.jpg" alt="Logo" className="novios" />
+//         </div>
+
+//         <div className="container-novios">
+//           <img src="/assets/foto2.jpg" alt="Logo" className="novios" />
+//         </div>
+
+//         <div className="container-novios">
+//           <img src="/assets/foto3.jpg" alt="Logo" className="novios" />
+//         </div>
+
+//       </Slider>
+//     </Carrusel>
+//   );
+// };
+
+// export default Carrousel;
